59567a1a51b882d6938cb0c5adebd70064d58bd4
anekrasov <nekrasov.aa.w@gmail.com>
2023-06-18 22:48:40 +0300

    separating the main and DM_Boston_Crime.scala classes. adding a jar artifact for DM_Boston_Crime.scala

---
+++ src/main/scala/DM_Boston_Crime.scala
@@ -0,0 +1,53 @@
+import java.text.SimpleDateFormat
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{avg, count}
+
+object DM_Boston_Crime{
+
+  private def TS2Date():String = {
+    val ts = System.currentTimeMillis()
+    val df:SimpleDateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss_SSSZ")
+    df.format(ts)
+  }
+
+  def main(args: Array[String]): Unit = {
+    if (args.length < 2) {
+      System.err.println("Invalid args")
+      System.exit(1)
+    }
+
+    val in_path = args(0)
+    val out_path = args(1)
+    val HEADER_TRUE = true
+
+    val spark = SparkSession
+      .builder
+      .appName("DM_BostonCrime")
+      .getOrCreate()
+
+    val crime =
+      spark.read
+        .option("header",HEADER_TRUE)
+        .csv(in_path + "/crime.csv")
+
+    val offense_codes =
+      spark.read
+        .option("header",HEADER_TRUE)
+        .csv(in_path + "/offense_codes.csv")
+        .distinct
+
+    crime
+      .join(offense_codes, crime("OFFENSE_CODE") === offense_codes("CODE"),"leftouter")
+      .groupBy("DISTRICT")
+      .agg(count("*").alias("crimes_total"))
+      // .agg(percentile_approx("*",YEAR,MONTH)).alias("crimes_monthly")
+      // .agg(("*")).alias("frequent_crime_types")
+      // .agg(("*")).alias("crime_type")
+      .agg(avg("Lat")).alias("lat")
+      .agg(avg("Long")).alias("lng")
+      .write.parquet(out_path + "/datamart" + TS2Date() + ".parquet")
+
+    spark.stop()
+  }
+
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,54 +1,6 @@
-import java.text.SimpleDateFormat
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{avg, count}
 
 object Main{
-
-  def TS2Date():String = {
-    val ts = System.currentTimeMillis()
-    val df:SimpleDateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss_SSSZ")
-    df.format(ts)
-  }
-
   def main(args: Array[String]): Unit = {
-
-    if (args.length < 1) {
-      System.err.println("Usage: HdfsTest <file>")
-      System.exit(1)
-    }
-
-    var in_path = args(1)
-    var out_path = args(2)
-
-
-    val spark = SparkSession
-      .builder
-      .appName("DM_BostonCrime")
-      .getOrCreate()
-
-    val crime =
-      spark.read
-        .option("header",true)
-        .csv(in_path + "/crime.csv")
-
-    val offense_codes =
-      spark.read
-        .option("header",true)
-        .csv(in_path + "/offense_codes.csv")
-        .distinct
-
-    crime
-      .join(offense_codes, crime("OFFENSE_CODE") === offense_codes("CODE"),"leftouter")
-      .groupBy("DISTRICT")
-      .agg(count("*").alias("crimes_total"))
-      // .agg(percentile_approx("*",YEAR,MONTH)).alias("crimes_monthly")
-      // .agg(("*")).alias("frequent_crime_types")
-      // .agg(("*")).alias("crime_type")
-      .agg(avg("Lat")).alias("lat")
-      .agg(avg("Long")).alias("lng")
-      .write.parquet(out_path + "/datamart" + TS2Date() + ".parquet")
-
-    spark.stop()
+    DM_Boston_Crime.main(Array("/","/"))
   }
-
 }
